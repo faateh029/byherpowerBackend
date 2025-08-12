8b3dacd5e9858426f6d491f4da5231afdb6bc57f
faateh029 <buttfatih691@gmail.com>
2025-08-12 14:44:34 +0500

    test auth routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import{authRouter} from './routes/auth.routes.js';
 import  errorHandler from "./middleware/error.middleware.js";
 import connectDB from "./config/db.js";
 import requestLogger from "./middleware/requestLogger.middleware.js";
+import { cartRouter } from "./routes/cart.routes.js";
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(requestLogger);
-app.get("/", (req, res) => {
-    res.send("Server is running");
-});
+app.use("/auth", authRouter);
+app.use('/cart' , cartRouter);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
